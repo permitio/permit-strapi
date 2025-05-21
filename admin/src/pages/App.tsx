0a7b01b3bea150d0cd5b0bc3deb1d2ecb35b764b
Taofiqq <taofiqaiyelabegan45@gmail.com>
2025-05-21 14:46:54 +0100

    update

--- admin/src/pages/App.tsx
+++ admin/src/pages/App.tsx
@@ -2,12 +2,14 @@ import { Page } from '@strapi/strapi/admin';
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from './HomePage';
 import { ConfigPage } from './ConfigPage';
+import { Dashboard } from './Dashboard';
 
 const App = () => {
   return (
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="config" element={<ConfigPage />} />
+      <Route path="dashboard" element={<Dashboard />} />
       <Route path="*" element={<Page.Error />} />
     </Routes>
   );
